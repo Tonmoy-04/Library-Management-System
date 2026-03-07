d733a3d3cd8699b874748b88fabf948fb63401b9
Noman Ahmed Tonmoy <20noman.ahmed@gmail.com>
2026-03-08 02:45:09 +0600

    Frontend styling

---
+++ library-frontend/src/pages/readers/EditReader.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function EditReader() {
+  return <div>Edit Reader</div>;
+}
---
+++ library-frontend/src/pages/readers/Readers.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Readers() {
+  return <div>Readers List</div>;
+}
